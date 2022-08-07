a143277bb7abc7a7cedc29902199b492f68094e3
Jun Ru Chen <junru.chen.2007@gmail.com>
2022-08-07 19:34:52 -0400

    New post and site upgrades

--- source/load.js
+++ source/load.js
@@ -1,15 +1,26 @@
-const max = 1;
+const max = 2;
 function getmax() {
     return max;
 }
+
 function inval() {
     alert("Invalid page number, please try pages 1 to " + getmax());
 }
 
 function load(index) {
-    console.log(index);
     if (index > 0 && index <= getmax()) {
         window.location.href = "blog" + index + ".html";
     }
     else inval();
+}
+
+function draw(canvas, imgsrc, x, y, w, h) {
+    var img = new Image();
+    img.src = imgsrc;
+    img.onload = function () {
+        var ctx = document.getElementById(canvas)
+        ctx.width = w;
+        ctx.height = h;
+        ctx.getContext("2d").drawImage(img, x, y, w, h);
+    }
 }
