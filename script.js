59a769131800bfddf29c26fa86118a040ba6a41f
Jun Ru Chen <junru.chen.2007@gmail.com>
2022-09-27 14:01:33 -0400

    new blog and minor rewrites

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var pos = 0;
 var imglen = 1016;
 function slide(){
-    $('#clientsSlider').animate({backgroundPosition : '-=2px'}, 20, 'linear', slide);
+    $('#blogSlider').animate({backgroundPosition : '-=2px'}, 20, 'linear', slide);
     pos += 2;
     if (pos >= imglen) {
         pos = 0;
@@ -9,18 +9,18 @@ function slide(){
 }
 setTimeout(slide, 1000);  // I DON'T KNOW WHY, BUT IF THERE'S NO TIMEOUT, IT DOESN'T WORK
 
-function imgtoid(posx) {
-    if (posx < 180) return 1;
-    if (posx < 565) return 2;
-    if (posx < 1016) return 3;
-    else return imgtoid(posx - 1016);
+function imgtoidb(posx) {
+    var truex = posx % 1016
+    if (truex < 180) return 1;
+    if (truex < 565) return 2;
+    if (truex < 1016) return 3;
 }
 
 $(document).ready(function(event) {
-    $("#clientsSlider").click(function(event) {
-        var redirect = window.confirm("Do You Want to Access At Blog #" + imgtoid(pos + event.pageX - 25));
+    $("#blogSlider").click(function(event) {
+        var redirect = window.confirm("Do You Want to Access At Blog #" + imgtoidb(pos + event.pageX - 25));
         if (redirect) {
-            load(imgtoid(pos + event.pageX - 25));
+            load(imgtoidb(pos + event.pageX - 25));
         }
     });
     $("#search").keydown(function(event) {
